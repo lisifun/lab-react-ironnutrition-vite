37d255a09f2098d27d296beef6c0f34750bc4160
lisifun <lisandrafj99@gmail.com>
2023-12-30 11:27:57 -0500

    adding iteration 3

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -4,12 +4,12 @@ import React from "react";
 const FoodBox = ({ food }) => {
   const { name, calories, image, servings } = food;
   return (
-    <div>
+    <div style={{ border: "1px solid black", margin: "20px", padding: "20px" }}>
       <p>
         <b>{name}</b>
       </p>
 
-      <img src={image} />
+      <img src={image} style={{ width: "150px", height: "100px" }} />
 
       <p>Calories: {calories}</p>
       <p>Servings {servings}</p>
