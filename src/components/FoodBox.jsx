7281754bab00b9043c828c926d363f89e3fc730c
lisifun <lisandrafj99@gmail.com>
2023-12-30 11:23:18 -0500

    adding iteration 2

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,26 @@
 // Your code here
+import React from "react";
+
+const FoodBox = ({ food }) => {
+  const { name, calories, image, servings } = food;
+  return (
+    <div>
+      <p>
+        <b>{name}</b>
+      </p>
+
+      <img src={image} />
+
+      <p>Calories: {calories}</p>
+      <p>Servings {servings}</p>
+
+      <p>
+        <b>Total Calories: {servings * calories} </b> kcal
+      </p>
+
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export default FoodBox;
