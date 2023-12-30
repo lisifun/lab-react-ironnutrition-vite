03622a1ff82e57ac662d61379d91665278861261
lisifun <lisandrafj99@gmail.com>
2023-12-30 16:27:14 -0500

    adding iteration 5

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import foodsJson from "./foods.json";
 import FoodBox from "./components/FoodBox";
+import AddFoodForm from "./components/AddFoodForm";
 
 function App() {
   const [foods, setFoods] = useState(foodsJson);
@@ -13,9 +14,17 @@ function App() {
     setFoods(udpatedFood);
   };
 
+  const addNewFood = (newFood) => {
+    let udpatedFood = [newFood, ...foods];
+    setFoods(udpatedFood);
+  };
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+      <h3>Add Food Entry</h3>
+      <AddFoodForm addNewFood={addNewFood} />
+      <h3>Food List</h3>
       {foods.map((food, index) => {
         return (
           <FoodBox key={index} food={food} deleteThisFood={deleteThisFood} />
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,1 +1,86 @@
 // Your code here
+import React, { useState } from "react";
+
+const AddFoodForm = ({ addNewFood }) => {
+  const [newFood, setNewFood] = useState({
+    id: "",
+    name: "",
+    image: "",
+    calories: "",
+    servings: "",
+  });
+
+  const handleTextInput = (e) => {
+    setNewFood((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleNumberInput = (e) => {
+    setNewFood((prev) => ({
+      ...prev,
+      [e.target.name]: Number(e.target.value),
+    }));
+  };
+
+  return (
+    <div>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+
+          gap: "20px",
+        }}
+      >
+        <div>
+          <label>Name</label>
+          <input
+            name="name"
+            type="text"
+            value={newFood.name}
+            onChange={handleTextInput}
+          ></input>
+        </div>
+
+        <div>
+          <label>Image</label>
+          <input
+            name="image"
+            type="text"
+            value={newFood.image}
+            onChange={handleTextInput}
+          ></input>
+        </div>
+
+        <div>
+          <label>Calories</label>
+          <input
+            name="calories"
+            type="number"
+            value={newFood.calories}
+            onChange={handleNumberInput}
+          ></input>
+        </div>
+
+        <div>
+          <label>Servings</label>
+          <input
+            name="servings"
+            type="number"
+            value={newFood.servings}
+            onChange={handleNumberInput}
+          ></input>
+        </div>
+      </form>
+      <button
+        style={{ margin: "20px" }}
+        onClick={() => {
+          addNewFood(newFood);
+        }}
+      >
+        Create
+      </button>
+    </div>
+  );
+};
+
+export default AddFoodForm;
